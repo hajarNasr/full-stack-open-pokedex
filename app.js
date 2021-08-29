8a80e65b888607f8fd22203b0114ea39db562bf8
hajarNasr <hajar-nasr@hotmail.com>
2021-08-29 14:14:30 +0200

    feat: add health endpoint

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
